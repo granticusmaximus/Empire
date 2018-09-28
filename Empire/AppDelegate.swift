dc52819769e30983508beb00134299c58b44b22c
Grant Watson <grant@triumphdesigns.org>
2018-09-28 10:55:26 -0400

    Initial Commit

---
+++ Empire/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Empire
+//
+//  Created by Grant Watson on 9/28/18.
+//  Copyright © 2018 Grant Watson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Empire/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Empire
+//
+//  Created by Grant Watson on 9/28/18.
+//  Copyright © 2018 Grant Watson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
